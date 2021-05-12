aefcb4a06170b7a02543b32180cf01bbc10ef475
Aysan Isayo <aisayo@icloud.com>
2021-05-11 17:23:13 -0700

    lecture

---
+++ src/images.js
@@ -0,0 +1,7 @@
+const images = [
+    'https://miro.medium.com/max/618/1*0poukmfxSJxjiY3-b6CKIA.png',
+    'https://cdn.codecoda.com/images/made/images/breadcrumb/javascript-dom-manipulation-banner_1543_592_40.jpg',
+    'https://www.encodedna.com/javascript/javascript-createElement-method.png',
+    'https://i.ytimg.com/vi/6Na3gOTB8cE/maxresdefault.jpg'
+ ]
+ 
---
+++ src/index.js
@@ -0,0 +1,20 @@
+// select an element with id parent-node
+let parentNode = document.getElementById('parent-node')
+// document.querySelector('#parent-node')
+// document.body.firstElementChild
+
+// select the child node of parentNode
+// let childNode = 
+// parentNode.firstElementChild
+// parentNode.querySelector('div')
+
+// change to h1 text 
+// parentNode.querySelector('h1').innerText = "This is DOM Manipulation"
+
+// create a paragraph element & append to parentnode
+    // create a paragraph
+    // let p = document.createElement('p')
+
+    // // append to parentNode
+    // parentNode += p 
+    
