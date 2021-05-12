a49b63f365fe4c2a6556cb5e18264186b071e3cc
Aysan Isayo <aisayo@icloud.com>
2021-05-11 17:45:46 -0700

    added new code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 // select an element with id parent-node
+
 let parentNode = document.getElementById('parent-node')
 // document.querySelector('#parent-node')
 // document.body.firstElementChild
 
 // select the child node of parentNode
-// let childNode = 
-// parentNode.firstElementChild
+
+let childNode = parentNode.firstElementChild
 // parentNode.querySelector('div')
 
 // change to h1 text 
-// parentNode.querySelector('h1').innerText = "This is DOM Manipulation"
+    parentNode.querySelector('h1').innerText = "This is DOM Manipulation"
 
 // create a paragraph element & append to parentnode
     // create a paragraph
-    // let p = document.createElement('p')
+    let p = `<p></p>`
 
     // // append to parentNode
-    // parentNode += p 
+    parentNode.innerHTML += p 
     
